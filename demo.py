ff82a4b49766bf570c4ff039571c9964c06af2b8
slbm <shola.cambrian@gmail.com>
2025-08-10 10:31:13 +0100

    modified demo.py file

--- demo.py
+++ demo.py
@@ -1,1 +1,4 @@
-print("Jesu loba")
+print("Jesu loba lori gbogbo agbaye, gbogbo eniyan ni o ni " \
+"lati mọ pe Jesu Kristi ni Oluwa ati Olugbala. " \
+"A gbọdọ fi ifẹ ati aanu han si gbogbo eniyan, ki a le tan " \
+"imọlẹ rẹ kaakiri agbaye.")
